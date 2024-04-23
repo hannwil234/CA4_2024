64b1b11ced6b29563fb43adda4349f23eb67e867
MariusVik <82534568+MariusVik@users.noreply.github.com>
2024-04-23 17:03:43 +0200

    Add files via upload

---
+++ assigment4_ttk4192/scripts/hybrid_astar_old.py
@@ -0,0 +1,448 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import argparse
+import matplotlib.animation as animation
+from math import pi, tan, sqrt
+import matplotlib.pyplot as plt
+from matplotlib.collections import PatchCollection, LineCollection
+from matplotlib.patches import Rectangle
+from itertools import product
+import argparse
+from utils.grid import Grid
+from utils.car import SimpleCar
+from utils.environment import Environment
+from utils.dubins_path import DubinsPath
+from utils.astar import Astar
+from utils.utils import plot_a_car, get_discretized_thetas, round_theta, same_point
+
+
+""" ---------------------------------------------------------------
+ Code for pathfinding using hybrid A-star 
+ dependencies: Curve generator (Dubins path)
+ Based on: Open source scripts in GitHub
+ Date: 18.01.23
+--------------------------------------------------------------------
+"""
+
+class HybridAstar:
+    """ Hybrid A* search procedure. """
+    def __init__(self, car, grid, reverse, unit_theta=pi/12, dt=1e-2, check_dubins=1):
+        self.car = car
+        self.grid = grid
+        self.reverse = reverse
+        self.unit_theta = unit_theta
+        self.dt = dt
+        self.check_dubins = check_dubins
+
+        self.start = self.car.start_pos
+        self.goal = self.car.end_pos
+
+        self.r = self.car.l / tan(self.car.max_phi)
+        self.drive_steps = int(sqrt(2)*self.grid.cell_size/self.dt) + 1
+        self.arc = self.drive_steps * self.dt
+        self.phil = [-self.car.max_phi, 0, self.car.max_phi]
+        self.ml = [1, -1]
+
+        if reverse:
+            self.comb = list(product(self.ml, self.phil))
+        else:
+            self.comb = list(product([1], self.phil))
+
+        self.dubins = DubinsPath(self.car)
+        self.astar = Astar(self.grid, self.goal[:2])
+        
+        self.w1 = 0.95 # weight for astar heuristic
+        self.w2 = 0.05 # weight for simple heuristic
+        self.w3 = 0.30 # weight for extra cost of steering angle change
+        self.w4 = 0.10 # weight for extra cost of turning
+        self.w5 = 2.00 # weight for extra cost of reversing
+
+        self.thetas = get_discretized_thetas(self.unit_theta)
+    
+    def construct_node(self, pos):
+        """ Create node for a pos. """
+
+        theta = pos[2]
+        pt = pos[:2]
+
+        theta = round_theta(theta % (2*pi), self.thetas)
+        
+        cell_id = self.grid.to_cell_id(pt)
+        grid_pos = cell_id + [theta]
+
+        node = Node(grid_pos, pos)
+
+        return node
+    
+    def simple_heuristic(self, pos):
+        """ Heuristic by Manhattan distance. """
+
+        return abs(self.goal[0]-pos[0]) + abs(self.goal[1]-pos[1])
+        
+    def astar_heuristic(self, pos):
+        """ Heuristic by standard astar. """
+
+        h1 = self.astar.search_path(pos[:2]) * self.grid.cell_size
+        h2 = self.simple_heuristic(pos[:2])
+        
+        return self.w1*h1 + self.w2*h2
+
+    def get_children(self, node, heu, extra):
+        """ Get successors from a state. """
+
+        children = []
+        for m, phi in self.comb:
+
+            # don't go back
+            if node.m and node.phi == phi and node.m*m == -1:
+                continue
+
+            if node.m and node.m == 1 and m == -1:
+                continue
+
+            pos = node.pos
+            branch = [m, pos[:2]]
+
+            for _ in range(self.drive_steps):
+                pos = self.car.step(pos, phi, m)
+                branch.append(pos[:2])
+
+            # check safety of route-----------------------
+            pos1 = node.pos if m == 1 else pos
+            pos2 = pos if m == 1 else node.pos
+            if phi == 0:
+                safe = self.dubins.is_straight_route_safe(pos1, pos2)
+            else:
+                d, c, r = self.car.get_params(pos1, phi)
+                safe = self.dubins.is_turning_route_safe(pos1, pos2, d, c, r)
+            # --------------------------------------------
+            
+            if not safe:
+                continue
+            
+            child = self.construct_node(pos)
+            child.phi = phi
+            child.m = m
+            child.parent = node
+            child.g = node.g + self.arc
+            child.g_ = node.g_ + self.arc
+
+            if extra:
+                # extra cost for changing steering angle
+                if phi != node.phi:
+                    child.g += self.w3 * self.arc
+                
+                # extra cost for turning
+                if phi != 0:
+                    child.g += self.w4 * self.arc
+                
+                # extra cost for reverse
+                if m == -1:
+                    child.g += self.w5 * self.arc
+
+            if heu == 0:
+                child.f = child.g + self.simple_heuristic(child.pos)
+            if heu == 1:
+                child.f = child.g + self.astar_heuristic(child.pos)
+            
+            children.append([child, branch])
+
+        return children
+    
+    def best_final_shot(self, open_, closed_, best, cost, d_route, n=10):
+        """ Search best final shot in open set. """
+
+        open_.sort(key=lambda x: x.f, reverse=False)
+
+        for t in range(min(n, len(open_))):
+            best_ = open_[t]
+            solutions_ = self.dubins.find_tangents(best_.pos, self.goal)
+            d_route_, cost_, valid_ = self.dubins.best_tangent(solutions_)
+        
+            if valid_ and cost_ + best_.g_ < cost + best.g_:
+                best = best_
+                cost = cost_
+                d_route = d_route_
+        
+        if best in open_:
+            open_.remove(best)
+            closed_.append(best)
+        
+        return best, cost, d_route
+    
+    def backtracking(self, node):
+        """ Backtracking the path. """
+
+        route = []
+        while node.parent:
+            route.append((node.pos, node.phi, node.m))
+            node = node.parent
+        
+        return list(reversed(route))
+    
+    def search_path(self, heu=1, extra=False):
+        """ Hybrid A* pathfinding. """
+
+        root = self.construct_node(self.start)
+        root.g = float(0)
+        root.g_ = float(0)
+        
+        if heu == 0:
+            root.f = root.g + self.simple_heuristic(root.pos)
+        if heu == 1:
+            root.f = root.g + self.astar_heuristic(root.pos)
+
+        closed_ = []
+        open_ = [root]
+
+        count = 0
+        while open_:
+            count += 1
+            best = min(open_, key=lambda x: x.f)
+
+            open_.remove(best)
+            closed_.append(best)
+
+            # check dubins path
+            if count % self.check_dubins == 0:
+                solutions = self.dubins.find_tangents(best.pos, self.goal)
+                d_route, cost, valid = self.dubins.best_tangent(solutions)
+                
+                if valid:
+                    best, cost, d_route = self.best_final_shot(open_, closed_, best, cost, d_route)
+                    route = self.backtracking(best) + d_route
+                    path = self.car.get_path(self.start, route)
+                    cost += best.g_
+                    print('Shortest path: {}'.format(round(cost, 2)))
+                    print('Total iteration:', count)
+                    
+                    return path, closed_
+
+            children = self.get_children(best, heu, extra)
+
+            for child, branch in children:
+
+                if child in closed_:
+                    continue
+
+                if child not in open_:
+                    best.branches.append(branch)
+                    open_.append(child)
+
+                elif child.g < open_[open_.index(child)].g:
+                    best.branches.append(branch)
+
+                    c = open_[open_.index(child)]
+                    p = c.parent
+                    for b in p.branches:
+                        if same_point(b[-1], c.pos[:2]):
+                            p.branches.remove(b)
+                            break
+                    
+                    open_.remove(child)
+                    open_.append(child)
+
+        return None, None
+
+
+def main_hybrid_a(heu,start_pos, end_pos,reverse, extra, grid_on):
+
+    tc = map_grid()
+    env = Environment(tc.obs, 5, 2.9) 
+    car = SimpleCar(env, start_pos, end_pos, 0.1875)
+    grid = Grid(env, 0.1)
+
+    hastar = HybridAstar(car, grid, reverse)
+
+    t = time.time()
+    path, closed_ = hastar.search_path(heu, extra)
+    print('Total time: {}s'.format(round(time.time()-t, 3)))
+
+    if not path:
+        print('No valid path!')
+        return
+    # a post-processing is required to have path list
+    path = path[::5] + [path[-1]]
+    branches = []
+    bcolors = []
+    for node in closed_:
+        for b in node.branches:
+            branches.append(b[1:])
+            bcolors.append('y' if b[0] == 1 else 'b')
+
+    xl, yl = [], []
+    xl_np1,yl_np1=[],[]
+    carl = []
+    dt_s=int(25)  # samples for gazebo simulator
+    for i in range(len(path)):
+        xl.append(path[i].pos[0])
+        yl.append(path[i].pos[1])
+        carl.append(path[i].model[0])
+        if i==0 or i==len(path):
+            xl_np1.append(path[i].pos[0])
+            yl_np1.append(path[i].pos[1])            
+        elif dt_s*i<len(path):
+            xl_np1.append(path[i*dt_s].pos[0])
+            yl_np1.append(path[i*dt_s].pos[1])      
+    # defining way-points
+    xl_np=np.array(xl_np1)
+    yl_np=np.array(yl_np1)
+    xl_np2 = np.append(xl_np, end_pos[0])
+    yl_np2 = np.append(yl_np, end_pos[1])
+    list_of_waypoints = np.column_stack([xl_np2, yl_np2])
+    xl_np=xl_np-10
+    yl_np=yl_np-10
+    global WAYPOINTS
+    #WAYPOINTS=np.column_stack([xl_np,yl_np])
+    #print(WAYPOINTS)
+    print(list_of_waypoints)
+    #return list_of_waypoints
+    
+    start_state = car.get_car_state(car.start_pos)
+    end_state = car.get_car_state(car.end_pos)
+
+    # plot and annimation
+    fig, ax = plt.subplots(figsize=(6,6))
+    ax.set_xlim(0, env.lx)
+    ax.set_ylim(0, env.ly)
+    ax.set_aspect("equal")
+
+    if grid_on:
+        ax.set_xticks(np.arange(0, env.lx, grid.cell_size))
+        ax.set_yticks(np.arange(0, env.ly, grid.cell_size))
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+        ax.tick_params(length=0)
+        plt.grid(which='both')
+    else:
+        ax.set_xticks([])
+        ax.set_yticks([])
+    
+    for ob in env.obs:
+        ax.add_patch(Rectangle((ob.x, ob.y), ob.w, ob.h, fc='gray', ec='k'))
+    
+    ax.plot(car.start_pos[0], car.start_pos[1], 'ro', markersize=6)
+    ax = plot_a_car(ax, end_state.model)
+    ax = plot_a_car(ax, start_state.model)
+
+    _branches = LineCollection([], linewidth=1)
+    ax.add_collection(_branches)
+
+    _path, = ax.plot([], [], color='lime', linewidth=2)
+    _carl = PatchCollection([])
+    ax.add_collection(_carl)
+    _path1, = ax.plot([], [], color='w', linewidth=2)
+    _car = PatchCollection([])
+    ax.add_collection(_car)
+    
+    frames = len(branches) + len(path) + 1
+
+    def init():
+        _branches.set_paths([])
+        _path.set_data([], [])
+        _carl.set_paths([])
+        _path1.set_data([], [])
+        _car.set_paths([])
+
+        return _branches, _path, _carl, _path1, _car
+
+    def animate(i):
+
+        edgecolor = ['k']*5 + ['r']
+        facecolor = ['y'] + ['k']*4 + ['r']
+
+        if i < len(branches):
+            _branches.set_paths(branches[:i+1])
+            _branches.set_color(bcolors)
+        
+        else:
+            _branches.set_paths(branches)
+
+            j = i - len(branches)
+
+            _path.set_data(xl[min(j, len(path)-1):], yl[min(j, len(path)-1):])
+
+            sub_carl = carl[:min(j+1, len(path))]
+            _carl.set_paths(sub_carl[::4])
+            _carl.set_edgecolor('k')
+            _carl.set_facecolor('m')
+            _carl.set_alpha(0.1)
+            _carl.set_zorder(3)
+
+            _path1.set_data(xl[:min(j+1, len(path))], yl[:min(j+1, len(path))])
+            _path1.set_zorder(3)
+
+            _car.set_paths(path[min(j, len(path)-1)].model)
+            _car.set_edgecolor(edgecolor)
+            _car.set_facecolor(facecolor)
+            _car.set_zorder(3)
+
+        return _branches, _path, _carl, _path1, _car
+
+    ani = animation.FuncAnimation(fig, animate, init_func=init, frames=frames,
+                                  interval=1, repeat=True, blit=True)
+
+    plt.show()
+    return list_of_waypoints
+
+class Node:
+    """ Hybrid A* tree node. """
+
+    def __init__(self, grid_pos, pos):
+
+        self.grid_pos = grid_pos
+        self.pos = pos
+        self.g = None
+        self.g_ = None
+        self.f = None
+        self.parent = None
+        self.phi = 0
+        self.m = None
+        self.branches = []
+
+    def __eq__(self, other):
+
+        return self.grid_pos == other.grid_pos
+    
+    def __hash__(self):
+
+        return hash((self.grid_pos))
+    
+class map_grid:
+    """ Here the obstacles are defined for a 20x20 map. """
+    def __init__(self):
+
+        self.obs = [
+            #[x, y, bredde, høyde]
+            #[1, 1.6, 1.7, 0.8],
+            #[3.2, 1.8, 0.8, 0.5],
+            #[1.3, 0.7, 0.9, 0.3],
+            #[2.4, 0.7, 0.9, 0.3],
+            #[0, 1, 0.5, 0.2],
+            #[2.7, 0, 2.3, 0.3]
+
+            [ 3.15, 0, 1.85, 0.3],
+            [0, 1, 0.4, 0.2],
+            [1, 1.5, 0.4, 0.8],
+            [1+0.4+0.1, 1.5, 0.4, 0.8],            
+            [1+0.4+0.1+0.4+0.1, 1.5, 0.7, 0.8],
+            [1.4, 0.5, 0.9, 0.3],
+            [1.4+0.9+0.2, 0.5, 0.9, 0.3],
+            [3.25,1.75,0.8,0.5]
+        ]
+
+'''
+if __name__ == '__main__':
+    print("Executing hybrid A* algorithm")
+    p = argparse.ArgumentParser()
+    p.add_argument('-heu', type=int, default=1, help='heuristic type')  #A* heuristic
+    p.add_argument('-r', action='store_true', help='allow reverse or not')
+    p.add_argument('-e', action='store_true', help='add extra cost or not')
+    p.add_argument('-g', action='store_true', help='show grid or not')
+    args = p.parse_args()
+    start_pos = [1, 1, 0]      # Here defined initial position [x,y,angle]
+    end_pos = [18, 18, 1*pi/4] # Target point [x,y, angle]
+    main_hybrid_a(args.heu,start_pos,end_pos,args.r,args.e,args.g)
+    print("An optimal path was computed using hybrid A* algorithm")
+'''
